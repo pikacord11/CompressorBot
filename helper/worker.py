041a02c4f1343052228f5cb919d3dba74293d4be
Pikacord <arunkapil01@gmail.com>
2022-09-11 11:05:06 +0530

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -64,8 +64,7 @@ async def stats(e):
     except BaseException:
         await e.answer("Someting Went Wrong 🤔\nResend Media", cache_time=0, alert=True)
 
-
-async def encc(e):
+async def removee(e):
     try:
         es = dt.now()
         COUNT.append(e.chat_id)
@@ -81,6 +80,76 @@ async def encc(e):
         )
         cmd = f'ffmpeg -i "{dl}" -map 0 -map -0:a:0 -c copy "{out}" -y'
         #cmd = f'ffmpeg -i "{dl}" -preset ultrafast -vf scale=640:360 -c:v libx265 -crf 28 -map 0:v -c:a libopus -ab 64k -map 0:a -c:s copy -map 0:s? "{out}" -y'
+        process = await asyncio.create_subprocess_shell(
+            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+        stdout, stderr = await process.communicate()
+        er = stderr.decode()
+        try:
+            if er:
+                await e.edit(str(er) + "\n\n**ERROR** Contact @danish_00")
+                COUNT.remove(e.chat_id)
+                os.remove(dl)
+                return os.remove(out)
+        except BaseException:
+            pass
+        ees = dt.now()
+        ttt = time.time()
+        await nn.delete()
+        nnn = await e.client.send_message(e.chat_id, "`Uploading...`")
+        with open(out, "rb") as f:
+            ok = await upload_file(
+                     client=e.client,
+                     file=f,
+                     name=out,
+                     progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                         progress(d, t, nnn, ttt, "uploading..")
+                         ),
+                     )
+        ds = await e.client.send_file(
+            e.chat_id,
+            file=ok,
+            force_document=True,
+            thumb=thum)
+        await nnn.delete()
+        org = int(Path(dl).stat().st_size)
+        com = int(Path(out).stat().st_size)
+        pe = 100 - ((com / org) * 100)
+        per = str(f"{pe:.2f}") + "%"
+        eees = dt.now()
+        x = dtime
+        xx = ts(int((ees - es).seconds) * 1000)
+        xxx = ts(int((eees - ees).seconds) * 1000)
+        a1 = await info(dl, e)
+        a2 = await info(out, e)
+        dk = await ds.reply(
+            f"Original Size : {hbs(org)}\nCompressed Size : {hbs(com)}\nCompressed Percentage : {per}\n\nMediainfo: [Before]({a1})//[After]({a2})\n\nDownloaded in {x}\nCompressed in {xx}\nUploaded in {xxx}",
+            link_preview=False,
+        )
+        await ds.forward_to(LOG)
+        await dk.forward_to(LOG)
+        COUNT.remove(e.chat_id)
+        os.remove(dl)
+        os.remove(out)
+    except Exception as er:
+        LOGS.info(er)
+        return COUNT.remove(e.chat_id)
+        
+async def encc(e):
+    try:
+        es = dt.now()
+        COUNT.append(e.chat_id)
+        wah = e.pattern_match.group(1).decode("UTF-8")
+        wh = decode(wah)
+        out, dl, thum, dtime = wh.split(";")
+        nn = await e.edit(
+            "`Compressing..`",
+            buttons=[
+                [Button.inline("STATS", data=f"stats{wah}")],
+                [Button.inline("CANCEL PROCESS", data=f"skip{wah}")],
+            ],
+        )
+        cmd = f'ffmpeg -i "{dl}" -preset ultrafast -vf scale=640:360 -c:v libx265 -crf 28 -map 0:v -c:a libopus -ab 64k -map 0:a -c:s copy -map 0:s? "{out}" -y'
         process = await asyncio.create_subprocess_shell(
             cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
